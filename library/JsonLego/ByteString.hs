2cb61876ea27054aba5a4b5faef5ec767ff59a26
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-10-10 02:03:22 +0300

    Ditch the thru-bytestring encoding of strings

--- library/JsonLego/ByteString.hs
+++ library/JsonLego/ByteString.hs
@@ -8,7 +8,6 @@ import qualified Data.Text.Encoding as TextEncoding
 import qualified Data.ByteString.Lazy as Lazy
 import qualified Data.ByteString.Builder as Builder
 import qualified Data.ByteString.Builder.Extra as Builder
-import qualified JsonLego.BoundedPrim as BoundedPrim
 import qualified Data.ByteString.Builder.Scientific as ScientificBuilder
 
 
@@ -16,19 +15,6 @@ builderWithStrategy strategy builder =
   builder
     & Builder.toLazyByteStringWith strategy Lazy.empty
     & Lazy.toStrict
-
-jsonStringBody :: Text -> ByteString
-jsonStringBody text =
-  text
-    & TextEncoding.encodeUtf8BuilderEscaped BoundedPrim.stringEncodedByte
-    & Builder.toLazyByteStringWith strategy Lazy.empty
-    & Lazy.toStrict
-  where
-    strategy =
-      Builder.untrimmedStrategy bufferSize bufferSize
-      where
-        bufferSize =
-          Text.length text * 2
 
 scientific :: Scientific -> ByteString
 scientific sci =
