9d3d9c87e24c5d059f6da26842508c71ba1bd5ff
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-10-12 17:45:18 +0300

    Clean up the benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,14 +3,11 @@ module Main where
 import Prelude
 import Gauge.Main
 import qualified Main.Model
-import qualified Main.JsonBytesBuilder
 import qualified Main.JsonLego
 import qualified Main.Aeson
 import qualified Data.Aeson
 import qualified Data.ByteString.Lazy
 import qualified Data.ByteString.Char8 as Char8ByteString
-import qualified JSONBytesBuilder.ByteString.ByteString
-import qualified JSONBytesBuilder.ByteString.LazyByteString
 import qualified JsonLego
 
 
@@ -23,17 +20,9 @@ main =
     test "aeson" encodeWithAeson input
 
     deepseq input $ defaultMain [
-      bgroup "strict" [
-        bench "json-lego" (nf encodeWithJsonLego input)
-        ,
-        bench "aeson" (nf encodeWithAeson input)
-        ]
+      bench "json-lego" (nf encodeWithJsonLego input)
       ,
-      bgroup "lazy" [
-        bench "json-lego" (nf (Data.ByteString.Lazy.fromStrict . encodeWithJsonLego) input)
-        ,
-        bench "aeson" (nf Data.Aeson.encode input)
-        ]
+      bench "aeson" (nf encodeWithAeson input)
       ]
 
 load :: FilePath -> IO Main.Model.Result
